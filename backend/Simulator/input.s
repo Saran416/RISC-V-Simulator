e9430e15df88d75724744b6b6d267e20d051c563
Saran416 <saisriramasarankonala@gmail.com>
2024-11-16 13:33:21 +0530

    fix: improved styling

--- backend/Simulator/input.s
+++ backend/Simulator/input.s
@@ -1,6 +1,1 @@
-.data
-.text
-addi x1, x0, 0x12
-slli x1, x1, 5
-lui x3, 0x10
 sd x1, 0(x3)
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Editor from '../components/Editor'
 import Navbar from '../components/Navbar'
 import Registers from '../components/Registers'
+import { useEffect, useState } from 'react'
 import './Home.css'
 
 
@@ -16,14 +17,30 @@ const Home = () => {
   // }
 
   // const runFile = async () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 1330);
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setIsSmallScreen(window.innerWidth <= 1330);
+    });
+  });
 
   return (
     <>
-    <Navbar></Navbar>
-    <div className="main-area">
-        <Editor className='editor-home'/>
-        <Registers  className='registers'/>
-    </div>
+      <Navbar />
+      <div className="main-area">
+        {isSmallScreen ? (
+          <>
+            <Registers className='registers' />
+            <Editor className='editor-home' />
+          </>
+        ) : (
+          <>
+            <Editor className='editor-home' />
+            <Registers className='registers' />
+          </>
+        )}
+      </div>
     </>
   )
 }
