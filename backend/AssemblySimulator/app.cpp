6d8ed801d177c967183d8dcd934e4cbc095df3ee
lakshsidhu04 <cs23btech11031@iith.ac.in>
2024-12-09 11:03:53 +0530

    fix: changes in makefile

--- backend/AssemblySimulator/app.cpp
+++
@@ -1,7 +0,0 @@
-#include "assembler.h"
-#include <iostream>
-
-int main(){
-
-    int res = convert();
-}
--- backend/Simulator/input.s
+++ backend/Simulator/input.s
@@ -1,9 +1,1 @@
-.data
-.dword 12
-.text
-addi x5, x0, 0xab
-add x4, x5, x0
-lui x3, 0x10   
-ld x6, 0(x3)
-sd x6, 24(x3)
-slli x5, x5, 8
+add x4,x5,x1
